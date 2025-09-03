6b67b9a5914981053d36dbcf536e68ee57edbfd3
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-03 13:58:11 +0800

    demo Tasklist component to be exported...

---
+++ src/Tasklist.js
@@ -0,0 +1,10 @@
+export default function TaskList() {
+    return (
+      <ul>
+        <li>Learn React</li>
+        <li>Build a Task List</li>
+        <li>Celebrate 🎉</li>
+      </ul>
+    );
+  }
+  
