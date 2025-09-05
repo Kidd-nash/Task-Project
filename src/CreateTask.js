418825731d2c4332c04a468fafa6a20503b39ef5
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-05 14:42:31 +0800

    Create Task Form

---
+++ src/CreateTask.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function CreateTask() {
+    return (
+        <form>
+          <input type="text" />
+          <button type="submit">Add Task</button>
+        </form>
+    );
+}
+
+export default CreateTask;
