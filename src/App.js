901cf917edd906758d0a90a7308f896e1182954e
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-05 13:34:58 +0800

    created a constant of task list and imported the Tasklist function component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import TaskList from "./Tasklist";
 
 function App() {
+  const tasks = [
+    { id: 1, text: "\"I can't do this\", \"I can't do it\" that's balogna." },
+    { id: 2, text: "Talent is a pursued interest." },
+    { id: 3, text: "Anything that you are willing to practice you can do." },
+    { id: 4, text: "- Bob Ross" }
+  ];
+
   return (
     <div>
       <h1>My Task List</h1>
-      <TaskList />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
