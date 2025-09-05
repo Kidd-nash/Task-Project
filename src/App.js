242c6ee85196e1f57ddc3ddc952313d5a6b99afb
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-05 13:49:22 +0800

    added useState for the initial tasks, setTask function to update the the state...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { useState } from "react";
 import TaskList from "./Tasklist";
 
 function App() {
-  const tasks = [
+  const [tasks, setTasks] = useState([    
     { id: 1, text: "\"I can't do this\", \"I can't do it\" that's balogna." },
     { id: 2, text: "Talent is a pursued interest." },
     { id: 3, text: "Anything that you are willing to practice you can do." },
     { id: 4, text: "- Bob Ross" }
-  ];
+  ]);
 
   return (
     <div>
