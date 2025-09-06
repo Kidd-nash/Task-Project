23f8fa23e2a68f935945e826f758a2dbc6c93b8a
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-06 12:26:56 +0800

    delete task call on tasklist, and button for display...

--- src/Tasklist.js
+++ src/Tasklist.js
@@ -1,9 +1,10 @@
-function TaskList({ tasks }) {
+function TaskList({ tasks, deleteTask }) {
     return (
       <ul className="list-group">
         {tasks.map(task => (
           <li key={task.id} className="list-group-item">
             {task.text}
+            <button onClick={() => { deleteTask(task.id) }}>X</button>
           </li>
         ))}
       </ul>
