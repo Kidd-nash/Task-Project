3173a3a52f3875e08add8cf31cc3f1d1ddcf50de
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-11 09:57:19 +0800

    default remiders list, usestate and useeffect for pushing and retrieving in web local storage

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function AppTwo() {
 
+    const defaultReminders = [
+        { id: 1, text: "First reminder, lorem ipsum dolor", date: new Date("2025-09-12T10:00:00")},
+        { id: 2, text: "Second reminder, lorem ipsum dolor", date: new Date("2025-09-15T15:30:00") },
+        { id: 3, text: "Third reminder, lorem ipsum dolor", date: new Date("2025-09-16T16:30:00") },
+    ];
+
+    const [reminders, setReminders] = useState(() => {
+        const savedReminders = localStorage.getItem("reminders");
+        return savedReminders ? JSON.parse(savedReminders) : defaultReminders;
+    });
+
+    useEffect(() => {
+        localStorage.setItem("reminders", JSON.stringify(reminders));
+    }, [reminders]);
+
     return (
-        <div>Reminder App</div>
+        <div className="container my-4 bg-light">
+            <div>Reminder App</div>
+        </div>
     )
 };
 
