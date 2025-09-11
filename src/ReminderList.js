60f2725b468905f897aeef62424169a1aaffadc5
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-11 17:24:50 +0800

    delete function prop/params, do not forget the proper syntax of using a function when parameters is required...

--- src/ReminderList.js
+++ src/ReminderList.js
@@ -1,10 +1,32 @@
-function ReminderList({ reminders }) {
+function ReminderList({ reminders, deleteReminder }) {
     return (
         <ul className="list-group">
             {reminders.map(reminder => (
                 <li className="list-group-item d-flex flex-row justify-content-between">
                     <span>{reminder.text}</span>
-                    <span>{reminder.date}</span>
+                    <div>
+                        <span>{reminder.date}</span>
+                        <div className="dropdown dropdown-start">
+                            <button
+                                type="button"
+                                className="btn btn-outline-secondary dropdown-toggle bg-white"
+                                data-bs-toggle="dropdown"
+                            >
+                                ...
+                            </button>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <button 
+                                        type="button"
+                                        className="dropdown-item text-danger"
+                                        onClick={() => deleteReminder(reminder.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </li>
                 ))
             };
