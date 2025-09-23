af8bcf9920c2f07d660b92e9641d72ce1cc681bc
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-23 14:56:17 +0800

    link/route to task form api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AppOne from "./AppOne";
 import AppTwo from "./AppTwo";
 import Trials from "./Trials";
+import AppTaskAPI from "./AppTaskAPI";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Link to="/app1" className="btn btn-primary m-2">App One</Link>
       <Link to="/app2" className="btn btn-secondary m-2">App Two</Link>
       <Link to="/trials" className="btn btn-warning m-2">Trials</Link>
+      <Link to="/api-tasks" className="btn btn-primary m-2">API Tasks</Link>
     </div>
   );
 }
@@ -21,6 +23,7 @@ function App() {
         <Route path="/app1" element={<AppOne />} />
         <Route path="/app2" element={<AppTwo />} />
         <Route path="/trials" element={<Trials />}/>
+        <Route path="/api-tasks" element={<AppTaskAPI />} />
       </Routes>
     </Router>
   );
