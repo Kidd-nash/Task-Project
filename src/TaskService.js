ae2a4dc41fc8f075bd355e5cd7045da35ed5078b
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-24 22:47:33 +0800

    deleteTask function, fetch request, method delete, data type, passing data in json

--- src/TaskService.js
+++ src/TaskService.js
@@ -3,13 +3,23 @@ const taskUrl = "http://localhost:8080/tasks";
 
 export function getTask() {
 
-return fetch(taskUrl)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Failed to fetch tasks");
-        }
-        return res.json();
-      })
-      .catch((err) => console.log(err.message));
+  return fetch(taskUrl)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to fetch tasks");
+      }
+      return res.json();
+    })
+    .catch((err) => console.log(err.message));
 
+}
+
+export async function deleteTask(id) {
+  const res = await fetch(`${taskUrl}?id=${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
     }
+  });
+  return res.json();
+}
