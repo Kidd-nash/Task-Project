ee6a51614587b1baf5b3ce7134c9229cb23770eb
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-25 11:11:01 +0800

    changeable url for api data between tasks and reminders

--- src/TaskService.js
+++ src/TaskService.js
@@ -1,12 +1,23 @@
-const taskUrl = "http://localhost:8080/tasks";
+
+function getUrl(type) {
+
+  if (type === "task") {
+    return "http://localhost:8080/tasks";
+  } if (type === "reminder") {
+    return "http://localhost:8080/api-data/reminders";
+  } else {
+    throw new Error("API data type undefined");
+  };
+};
 
 
-export function getTask() {
+export function getData(type) {
+  const apiUrl = getUrl(type);
 
-  return fetch(taskUrl)
+  return fetch(apiUrl)
     .then((res) => {
       if (!res.ok) {
-        throw new Error("Failed to fetch tasks");
+        throw new Error("Failed to fetch " + type);
       }
       return res.json();
     })
@@ -14,12 +25,12 @@ export function getTask() {
 
 }
 
-export async function deleteTask(id) {
-  const res = await fetch(`${taskUrl}?id=${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-  return res.json();
-}
+// export async function deleteTask(id) {
+//   const res = await fetch(`${taskUrl}?id=${id}`, {
+//     method: "DELETE",
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   });
+//   return res.json();
+// }
